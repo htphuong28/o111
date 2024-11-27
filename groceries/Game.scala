1dde84312dbb9f1fd5c9a8a1089e2a596c2f6701
htphuong28 <thieuphuong5knt@gmail.com>
2024-11-27 11:21:03 +0200

    add turns

--- groceries/Game.scala
+++ groceries/Game.scala
@@ -6,6 +6,7 @@ class Game:
   val player: Player = Player()
   private val map: Area = Area.map
   private val playerPath: mutable.Buffer[Direction] = mutable.Buffer.empty
+  private var turnsLeft = 30
 
   def playerArea: Area =
     this.map.traverse(this.playerPath.toVector) match
@@ -13,8 +14,10 @@ class Game:
       case None => ???
 
   def move(direction: Direction): Boolean =
-    if this.playerArea.neighbors.contains(direction) then
+    if this.turnsLeft <= 0 then false
+    else if this.playerArea.neighbors.contains(direction) then
       this.playerPath.append(direction)
+      this.turnsLeft -= 1
       true
     else false
 
@@ -23,5 +26,14 @@ class Game:
 
   def drop(idx: Int): Boolean =
     this.player.removeItem(idx).map(this.playerArea.addItem).isDefined
+    
+  def hasWon: Boolean =
+    this.playerArea.name == "House" &&
+      this.player.shoppingList.forall(this.player.inventory.contains)
+    
+  def hasLost: Boolean =
+    this.turnsLeft <= 0 &&
+      (this.playerArea.name != "House" ||
+        !this.player.shoppingList.forall(this.player.inventory.contains))
 
 end Game
